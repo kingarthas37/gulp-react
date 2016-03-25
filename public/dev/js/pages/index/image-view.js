7b1c426a675a35fee05f80b15d28d7eea1e62724
ralf <kingarthas37@hotmail.com>
2016-03-25 17:46:34 +0800

    3.26

--- public/dev/js/pages/index/image-view.js
+++ public/dev/js/pages/index/image-view.js
@@ -7,12 +7,12 @@ var ImageView = React.createClass({
         return (
             <div className="image-view">
                     <div>
-                        <img src="/public/dist/images/index/image-view-1.jpg" alt=""/>
-                        <img src="/public/dist/images/index/image-view-2.jpg" alt=""/>
+                        <img src="http://kingarthas37.github.io/static/react/public/dist/images/index/image-view-1.jpg" alt=""/>
+                        <img src="http://kingarthas37.github.io/static/react/public/dist/images/index/image-view-2.jpg" alt=""/>
                     </div>
                     <div>
-                        <img src="/public/dist/images/index/image-view-3.jpg" alt=""/>
-                        <img src="/public/dist/images/index/image-view-4.jpg" alt=""/>
+                        <img src="http://kingarthas37.github.io/static/react/public/dist/images/index/image-view-3.jpg" alt=""/>
+                        <img src="http://kingarthas37.github.io/static/react/public/dist/images/index/image-view-4.jpg" alt=""/>
                     </div>
             </div>
         );
